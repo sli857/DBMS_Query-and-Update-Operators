625a70a69d9eb3241a443ac5e94e57682d588541
sli857 <sli857@wisc.edu>
2024-05-01 00:47:17 -0500

    let's go babe

--- delete.C
+++ delete.C
@@ -1,7 +1,6 @@
 #include "catalog.h"
 #include "query.h"
 
-
 /*
  * Deletes records from a specified relation.
  *
@@ -10,17 +9,76 @@
  * 	an error code otherwise
  */
 
-const Status QU_Delete(const string & relation, 
-		       const string & attrName, 
-		       const Operator op,
-		       const Datatype type, 
-		       const char *attrValue)
+const Status QU_Delete(const string &relation,
+					   const string &attrName,
+					   const Operator op,
+					   const Datatype type,
+					   const char *attrValue)
 {
-// part 6
-return OK;
+	if (relation.empty())
+	{
+		return BADCATPARM;
+	}
 
+	HeapFileScan *heapScanner;
+	Status opStatus;
+	AttrDesc attributeDetails;
+	RID recordID;
+	int intVal;
+	float floatVal;
+	cout << "Delete!" << endl;
+	heapScanner = new HeapFileScan(relation, opStatus);
+	if (opStatus != OK)
+	{
+		return opStatus;
+	}
 
+	if (attrName.empty())
+	{
+		opStatus = heapScanner->startScan(0, 0, STRING, nullptr, EQ);
+	}
+	else
+	{
+		opStatus = attrCat->getInfo(relation, attrName, attributeDetails);
+		if (opStatus != OK)
+		{
+			delete heapScanner;
+			return opStatus;
+		}
 
+		switch (type)
+		{
+		case INTEGER:
+			intVal = atoi(attrValue);
+			opStatus = heapScanner->startScan(attributeDetails.attrOffset, attributeDetails.attrLen, INTEGER, reinterpret_cast<const char *>(&intVal), op);
+			break;
+		case FLOAT:
+			floatVal = atof(attrValue);
+			opStatus = heapScanner->startScan(attributeDetails.attrOffset, attributeDetails.attrLen, FLOAT, reinterpret_cast<const char *>(&floatVal), op);
+			break;
+		default:
+			opStatus = heapScanner->startScan(attributeDetails.attrOffset, attributeDetails.attrLen, STRING, attrValue, op);
+		}
+	}
+
+	if (opStatus != OK)
+	{
+		delete heapScanner;
+		return opStatus;
+	}
+
+	while ((opStatus = heapScanner->scanNext(recordID)) == OK)
+	{
+		opStatus = heapScanner->deleteRecord();
+		if (opStatus != OK)
+		{
+			delete heapScanner;
+			return opStatus;
+		}
+	}
+
+	heapScanner->endScan();
+	delete heapScanner;
+
+	return (opStatus == FILEEOF) ? OK : opStatus;
 }
-
-
--- insert.C
+++ insert.C
@@ -1,7 +1,6 @@
 #include "catalog.h"
 #include "query.h"
 
-
 /*
  * Inserts a record into the specified relation.
  *
@@ -10,12 +9,76 @@
  * 	an error code otherwise
  */
 
-const Status QU_Insert(const string & relation, 
-	const int attrCnt, 
-	const attrInfo attrList[])
+const Status QU_Insert(const string &relation,
+					   const int attrCnt,
+					   const attrInfo attrList[])
 {
-// part 6
-return OK;
+	Status status;
+	RID tupleID;
+	AttrDesc *schemaAttributes;
+	int schemaAttrCount;
+	int recordSize = 0;
+	char *recordData;
+	cout << "Insert!" << endl;
+	status = attrCat->getRelInfo(relation, schemaAttrCount, schemaAttributes);
+	if (status != OK)
+	{
+		return status;
+	}
 
+	if (schemaAttrCount != attrCnt)
+	{
+		return BADSCANPARM;
+	}
+
+	for (int i = 0; i < schemaAttrCount; ++i)
+	{
+		recordSize += schemaAttributes[i].attrLen;
+	}
+
+	InsertFileScan insertionScan(relation, status);
+	if (status != OK)
+	{
+		return status;
+	}
+
+	recordData = (char *)malloc(recordSize);
+	if (!recordData)
+	{
+		return INSUFMEM;
+	}
+
+	memset(recordData, 0, recordSize);
+
+	bool attrFound;
+	for (int i = 0; i < schemaAttrCount; ++i)
+	{
+		attrFound = false;
+		for (int j = 0; j < attrCnt; ++j)
+		{
+			if (strcmp(attrList[j].attrName, schemaAttributes[i].attrName) == 0)
+			{
+				attrFound = true;
+				if (attrList[j].attrType != schemaAttributes[i].attrType)
+				{
+					free(recordData);
+					return ATTRTYPEMISMATCH;
+				}
+				void *dest = recordData + schemaAttributes[i].attrOffset;
+				memcpy(dest, attrList[j].attrValue, schemaAttributes[i].attrLen);
+				break;
+			}
+		}
+		if (!attrFound)
+		{
+			free(recordData);
+			return ATTRNOTFOUND;
+		}
+	}
+
+	Record newRecord = {recordData, recordSize};
+	status = insertionScan.insertRecord(newRecord, tupleID);
+	free(recordData);
+
+	return status;
 }
-
