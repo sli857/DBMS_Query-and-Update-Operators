30c4799bcf33a896892e702d15104df779c2bbc1
sli857 <sli857@wisc.edu>
2024-05-01 17:49:27 -0500

    insert and select

--- insert.C
+++ insert.C
@@ -1,7 +1,6 @@
 #include "catalog.h"
 #include "query.h"
 
-
 /*
  * Inserts a record into the specified relation.
  *
@@ -10,12 +9,77 @@
  * 	an error code otherwise
  */
 
-const Status QU_Insert(const string & relation, 
-	const int attrCnt, 
-	const attrInfo attrList[])
+const Status QU_Insert(const string &relation,
+					   const int attrCnt,
+					   const attrInfo attrList[])
 {
-// part 6
-return OK;
+	Status status;
+	RID rid;
+	AttrDesc *attrDesc;
+	int attrCount;
 
+
+
+	status = attrCat->getRelInfo(relation, attrCount, attrDesc);
+	if (status != OK)
+	{
+		return status;
+	}
+
+	if (attrCount != attrCnt)
+	{
+		return BADSCANPARM;
+	}
+
+	int recordSize = 0;
+	for (int i = 0; i < attrCount; ++i)
+	{
+		recordSize += attrDesc[i].attrLen;
+	}
+
+	InsertFileScan insertionScan(relation, status);
+	if (status != OK)
+	{
+		return status;
+	}
+
+	char *recordData = (char *)malloc(recordSize);
+	if (!recordData)
+	{
+		return UNIXERR;
+	}
+
+	memset(recordData, 0, recordSize);
+
+	bool attrFound;
+	for (int i = 0; i < attrCount; ++i)
+	{
+		attrFound = false;
+		for (int j = 0; j < attrCnt; ++j)
+		{
+			if (strcmp(attrList[j].attrName, attrDesc[i].attrName) == 0)
+			{
+				attrFound = true;
+				if (attrList[j].attrType != attrDesc[i].attrType)
+				{
+					delete recordData;
+					return ATTRTYPEMISMATCH;
+				}
+				void *dest = recordData + attrDesc[i].attrOffset;
+				memcpy(dest, attrList[j].attrValue, attrDesc[i].attrLen);
+				break;
+			}
+		}
+		if (!attrFound)
+		{
+			delete recordData;
+			return ATTRNOTFOUND;
+		}
+	}
+
+	Record newRecord = {recordData, recordSize};
+	status = insertionScan.insertRecord(newRecord, rid);
+	delete recordData;
+
+	return status;
 }
-
--- select.C
+++ select.C
@@ -1,15 +1,14 @@
 #include "catalog.h"
 #include "query.h"
 
-
 // forward declaration
-const Status ScanSelect(const string & result, 
-			const int projCnt, 
-			const AttrDesc projNames[],
-			const AttrDesc *attrDesc, 
-			const Operator op, 
-			const char *filter,
-			const int reclen);
+const Status ScanSelect(const string &result,
+						const int projCnt,
+						const AttrDesc projNames[],
+						const AttrDesc *attrDesc,
+						const Operator op,
+						const char *filter,
+						const int reclen);
 
 /*
  * Selects records from the specified relation.
@@ -19,30 +18,148 @@ const Status ScanSelect(const string & result,
  * 	an error code otherwise
  */
 
-const Status QU_Select(const string & result, 
-		       const int projCnt, 
-		       const attrInfo projNames[],
-		       const attrInfo *attr, 
-		       const Operator op, 
-		       const char *attrValue)
+const Status QU_Select(const string &result,
+					   const int projCnt,
+					   const attrInfo projNames[],
+					   const attrInfo *attr,
+					   const Operator op,
+					   const char *attrValue)
 {
-   // Qu_Select sets up things and then calls ScanSelect to do the actual work
-    cout << "Doing QU_Select " << endl;
-
+	// Qu_Select sets up things and then calls ScanSelect to do the actual work
+	cout << "Doing QU_Select " << endl;
+	if (projCnt == 0)
+	{
+		return OK;
+	}
+	// cout << "Select test point 1" << endl;
+	AttrDesc adProjNames[projCnt];
+	AttrDesc attrDesc;
+	Status status;
+	int reclen;
+	for (int i = 0; i < projCnt; i++)
+	{
+		status = attrCat->getInfo(projNames[i].relName, projNames[i].attrName, adProjNames[i]);
+		if (status != OK)
+		{
+			return status;
+		}
+	}
+	// cout << "Select test point 2" << endl;
+	if (attr != NULL)
+	{
+		status = attrCat->getInfo(attr->relName, attr->attrName, attrDesc);
+	}
+	else
+	{
+		attrDesc = adProjNames[0];
+	}
+	// cout << "Select test point 2.5" << endl;
+	if (status != OK)
+	{
+		return status;
+	}
+	// cout << "Select test point 3" << endl;
+	reclen = 0;
+	for (int i = 0; i < projCnt; i++)
+	{
+		reclen += adProjNames[i].attrLen;
+	}
+	// cout << "Select test point 4, type: " << endl;
+	void *dest;
+	int temInt;
+	float temFloat;
+	if (attrValue == NULL)
+	{
+		dest = NULL;
+	}
+	else
+	{
+		switch (attr->attrType)
+		{
+		case STRING:
+			status = ScanSelect(result, projCnt, adProjNames, &attrDesc, op, attrValue, reclen);
+			return status;
+			break;
+		case INTEGER:
+			temInt = atoi(attrValue);
+			dest = (void *)(&temInt);
+			break;
+		case FLOAT:
+			temFloat = atof(attrValue);
+			dest = (void *)(&temFloat);
+			break;
+		default:
+			// memcpy(dest, attrValue, attr->attrLen);
+			break;
+		}
+	}
+	// cout << "Select test point 5" << endl;
+	status = ScanSelect(result, projCnt, adProjNames, &attrDesc, op, (char *)dest, reclen);
+	// cout << "Select test point 6" << endl;
+	return status;
 }
 
-
-const Status ScanSelect(const string & result, 
+const Status ScanSelect(const string &result,
 #include "stdio.h"
 #include "stdlib.h"
-			const int projCnt, 
-			const AttrDesc projNames[],
-			const AttrDesc *attrDesc, 
-			const Operator op, 
-			const char *filter,
-			const int reclen)
+						const int projCnt,
+						const AttrDesc projNames[],
+						const AttrDesc *attrDesc,
+						const Operator op,
+						const char *filter,
+						const int reclen)
 {
-    cout << "Doing HeapFileScan Selection using ScanSelect()" << endl;
+	cout << "Doing HeapFileScan Selection using ScanSelect()" << endl;
 
+    Status status;
+    InsertFileScan insertionScan(result, status);
+    if (status != OK){ 
+      return status; 
+    }
+    //cout << "ScanSelect test point 1" << endl;
+    char data[reclen];
+    Record outrec;
+    outrec.data = (void *) data;
+    outrec.length = reclen;
+    if(attrDesc == NULL){
+      return OK;
+    }
+    HeapFileScan fileScan(string(attrDesc->relName), status);
+    if (status != OK){ 
+      return status; 
+    }
+    //cout << "ScanSelect test point 2" << endl;
+    status = fileScan.startScan(attrDesc->attrOffset, attrDesc->attrLen, (Datatype)attrDesc->attrType, filter, op);
+    if (status != OK){
+      return status; 
+    }
+    //cout << "ScanSelect test point 3" << endl;
+    RID rid;
+    Record temrec;
+    while (fileScan.scanNext(rid) == OK){
+        //cout << "ScanSelect test point 4_1" << endl;
+        status = fileScan.getRecord(temrec);
+        if (status != OK){
+          //cout << "ScanSelect error 3" << endl;
+          return status; 
+        }
 
+        int offset = 0;
+        //cout << "ScanSelect test point 4_2" << endl;
+        for (int i = 0; i < projCnt; i++){
+            memcpy(data + offset,
+                        (char *)temrec.data + projNames[i].attrOffset,
+                        projNames[i].attrLen);                    
+            offset += projNames[i].attrLen;
+        } 
+        RID outRID;
+        //cout << "ScanSelect test point 4_3" << endl;
+        status = insertionScan.insertRecord(outrec, outRID);
+        if (status != OK){
+          //cout << "ScanSelect error 4" << endl;
+          return status; 
+        }
+        //cout << "ScanSelect test point 4_4" << endl;
+    }
+    return OK;
 }
